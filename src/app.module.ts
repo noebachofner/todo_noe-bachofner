0eb71d92e0dec06b7af3e126e8aa63c18c6c9154
Noé Bachofner <noebachofner@lxrin.ch>
2026-01-14 08:20:53 +0100

    Setup:  Konfiguration für Swagger, TypeORM, Validation und Environment Variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: process.env.DB_DATABASE || 'data/todo.db',
+      entities: [__dirname + '/**/*. entity{.ts,.js}'],
+      synchronize: true, // Only Development
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,49 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+
+  // Globaler Prefix
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  // Swagger Configuration
+  const config = new DocumentBuilder()
+    .setTitle(configService.get<string>('SWAGGER_TITLE') || 'Todo API')
+    .setDescription(
+      configService.get<string>('SWAGGER_DESCRIPTION') || 'API Description',
+    )
+    .setVersion(configService.get<string>('SWAGGER_VERSION') || '1.0.0')
+    .setContact(
+      configService.get<string>('SWAGGER_AUTHOR_NAME') || 'Author undefined',
+      configService.get<string>('SWAGGER_AUTHOR_URL') || '',
+      configService.get<string>('SWAGGER_AUTHOR_EMAIL') || '',
+    )
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(
+    configService.get<string>('SWAGGER_DOC_PATH') || 'docs',
+    app,
+    document,
+  );
+
+  const port = configService.get<number>('PORT') || 3000;
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}/api`);
+  console.log(`Swagger docs available at: http://localhost:${port}/docs`);
 }
 bootstrap();
